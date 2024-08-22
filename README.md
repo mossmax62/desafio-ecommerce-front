0ecffd6ca9eb473ee7999fda505f3cfb6bf9a54a
Diego Hernández <dieherram@gmail.com>
2024-08-21 20:08:17 -0400

    update README.md

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
 Este proyecto representa la capa de front end del proyecto ecommerce, en el se despliega la informacion y proveen funcionalidades para interactuar con el backend API.
 
-Carlos Valderas se ha conectado exitosamente a este proyecto.
+Diego Hernandez se ha conectado exitosamente a este proyecto.
 
 Conexión de Darío Ocaranza
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Navigation from './components/layout/Navigation';
+import Navigation from './components/Layout/Navigation';
 import ProtectedRoute from './components/auth/ProtectedRoute' // Import the ProtectedRoute component
 
 
@@ -7,7 +7,7 @@ import './main.css'
 import SingIn from './components/auth/SingIn';
 import SingUp from './components/auth/SingUp';
 import Home from './pages/Home';
-import ShoppingCart from './components/cart/ShoppingCart';
+import ShoppingCart from './components/Cart/ShoppingCart';
 import Notifications from './pages/Notifications';
 import UserProfile from './pages/UserProfile';
 import ManageProductsView from './pages/ManageProducts';
