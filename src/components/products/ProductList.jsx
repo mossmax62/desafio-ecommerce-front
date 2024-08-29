c12255014145d1a2d3bda5b44c332ef77eb4f2ab
Diego Hernández <dieherram@gmail.com>
2024-08-29 19:36:13 -0400

    add onClick

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,10 +1,13 @@
-import { useProducts } from '../../contexts/ProductContext'
+import { ProductContext, useProducts } from '../../contexts/ProductContext'
 import { useCart } from '../../contexts/CartContext'
 import './ProductList.css'
+import { useContext } from 'react'
 
 const ProductList = () => {
   const { products } = useProducts()
   const { addToCart } = useCart()
+
+  const { handleSeeSelectedCar } = useContext(ProductContext)
 
   return (
     <>
@@ -15,11 +18,11 @@ const ProductList = () => {
               {products.map(product => (
                 <div key={product.id} className='card col-sm-2 card m-2 p-1 text-center'>
                   <div className='card-title'>
-                    <h3>{product.make}</h3>
-                    <h2>{product.model}</h2>
+                    <a href='' onClick={() => handleSeeSelectedCar(event, product.id)}><h3>{product.make}</h3></a>
+                    <a href='' onClick={() => handleSeeSelectedCar(event, product.id)}><h2>{product.model}</h2></a>
                   </div>
                   <div className='card-body'>
-                    <img className='card-img-top' src={product.image} alt={product.model} />
+                    <a href='' onClick={() => handleSeeSelectedCar(event, product.id)}><img className='card-img-top' src={product.image} alt={product.model} /></a>
                   </div>
                   <div className='card-text'>
                     $ {product.horsepower}
