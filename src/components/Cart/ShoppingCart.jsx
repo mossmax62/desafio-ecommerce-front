fe9de2cd03579337a981df29347181f23bfd5d0a
Maximiliano Mussuto <mossmax62@gmail.com>
2024-08-28 21:16:32 -0400

    fix cart

--- src/components/Cart/ShoppingCart.jsx
+++ src/components/Cart/ShoppingCart.jsx
@@ -1,41 +1,43 @@
-import { useCart } from "../../contexts/CartContext"
+import { useCart } from '../../contexts/CartContext'
 import '../products/ProductList.css'
 
-
 const ShoppingCart = () => {
-
-    const { cart, clearCart, removeFromCart } = useCart();
-    return (
-        <div className="container">
-            <div>
-                <h1>Shopping Cart</h1>
-                <div className="container product-list">
-                    <div className="row">
-                        {cart.map(product => (
-                            <div key={Math.random() * product.id} className="col-sm-2 card m-2 text-center">
-                                <div className='card-title'>
-                                    <h3>{product.make}</h3>
-                                    <h2>{product.model}</h2>
-                                </div>
-                                <div className='card-body'>
-                                    <img className='card-img-top' src={product.image} alt={product.model} />
-                                </div>
-                                <div className='card-text'>
-                                    $ {product.horsepower}
-                                </div>
-                                <button onClick={() => removeFromCart(product.id)} className='btn btn-danger'>Remove</button>
-                            </div>
-
-                        ))}
-                        <br />
-                    </div>
+  const { cart, clearCart, addToCart, removeFromCart } = useCart()
+  return (
+    <div className='container'>
+      <div>
+        <h1>Shopping Cart</h1>
+        <div className='container product-list'>
+          <div className='row'>
+            {cart.map(product => (
+              <div key={Math.random() * product.id} className='col-sm-2 card m-2 text-center'>
+                <div className='card-title'>
+                  <h3>{product.make}</h3>
+                  <h2>{product.model}</h2>
                 </div>
-            </div>
-            <div>
-                <button onClick={clearCart} className='btn btn-danger'>Clear Cart</button>
+                <div className='card-body'>
+                  <img className='card-img-top' src={product.image} alt={product.model} />
+                </div>
+                <div className='card-text'>
+                  Cantidad : $ {product.quantity}
+                </div>
+                <div className='card-text'>
+                  $ {product.horsepower * product.quantity}
+                </div>
+                <button onClick={() => addToCart(product)} className='btn btn-primary'>Add to Cart</button>
+                <button onClick={() => removeFromCart(product.id)} className='btn btn-danger'>Remove</button>
+              </div>
 
-            </div>
+            ))}
+            <br />
+          </div>
         </div>
-    );
+      </div>
+      <div>
+        <button onClick={clearCart} className='btn btn-danger'>Clear Cart</button>
+
+      </div>
+    </div>
+  )
 }
-export default ShoppingCart;
+export default ShoppingCart
--- src/components/menu/Navigation.jsx
+++ src/components/menu/Navigation.jsx
@@ -1,69 +1,72 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import NotificationBell from './NotificationBell';
-import CartIcon from './CartIcon';
-import { useAuth } from '../../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import { useCart } from '../../contexts/CartContext';
-
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import NotificationBell from './NotificationBell'
+import CartIcon from './CartIcon'
+import { useAuth } from '../../contexts/AuthContext'
+import { useCart } from '../../contexts/CartContext'
 
 const Navigation = () => {
-    const { isAuthenticated, logout } = useAuth();
-    const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuth()
+  const navigate = useNavigate()
 
-    const { cart } = useCart();
+  const { cart } = useCart()
 
-    const handleLogout = () => {
-        console.log('Logout');
-        logout();
-        navigate('/');
-    }
+  let totalProductos = 0
+  cart.forEach(producto => {
+    totalProductos += producto.quantity
+  })
 
+  const handleLogout = () => {
+    console.log('Logout')
+    logout()
+    navigate('/')
+  }
 
-    return (
-        <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href='/' >DESAFIO ECOMMERCE</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto"
-                            navbarScroll>
-                            <Link to="/" className="nav-link btn btn-warning m-1">Inicio</Link>
-                            <Link to="/notifications" className="nav-link btn btn-warning m-1"><NotificationBell count={3} /> </Link>
-                            <Link to="/shopping-cart" className="nav-link btn btn-warning m-1"><CartIcon itemCount={cart.length} /></Link>
+  return (
+    <>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Navbar.Brand href='/'>DESAFIO ECOMMERCE</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav
+              className='ms-auto'
+              navbarScroll
+            >
+              <Link to='/' className='nav-link btn btn-warning m-1'>Inicio</Link>
+              <Link to='/notifications' className='nav-link btn btn-warning m-1'><NotificationBell count={3} /> </Link>
+              <Link to='/shopping-cart' className='nav-link btn btn-warning m-1'><CartIcon itemCount={totalProductos} /></Link>
 
-                            {!isAuthenticated ?
-                                <NavDropdown title="Acceso" id="basic-nav-dropdown" className="nav-link btn btn-warning m-1 p-1">
-                                    <NavDropdown.Item href='/sign-in' className="nav-link btn btn-warning" >
-                                        Sign In
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item href='/sign-up' className="nav-link btn btn-warning">
-                                        {/* <Link to="/sign-up" className="nav-link btn btn-warning">Sign Up</Link> */}
-                                        Sign Up
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                                :
-                                <NavDropdown title="Mi cuenta" id="basic-nav-dropdown" className="nav-link btn btn-warning m-1 p-1">
-                                    <NavDropdown.Item href='/user-profile' className="nav-link btn btn-warning" >
-                                        Profile
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item href='/manage-products' className="nav-link btn btn-warning" >
-                                        Manage Products
-                                    </NavDropdown.Item>
+              {!isAuthenticated
+                ? <NavDropdown title='Acceso' id='basic-nav-dropdown' className='nav-link btn btn-warning m-1 p-1'>
+                  <NavDropdown.Item href='/sign-in' className='nav-link btn btn-warning'>
+                    Sign In
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='/sign-up' className='nav-link btn btn-warning'>
+                    {/* <Link to="/sign-up" className="nav-link btn btn-warning">Sign Up</Link> */}
+                    Sign Up
+                  </NavDropdown.Item>
+                </NavDropdown>
+                : <NavDropdown title='Mi cuenta' id='basic-nav-dropdown' className='nav-link btn btn-warning m-1 p-1'>
+                  <NavDropdown.Item href='/user-profile' className='nav-link btn btn-warning'>
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='/manage-products' className='nav-link btn btn-warning'>
+                    Manage Products
+                  </NavDropdown.Item>
 
-                                    <NavDropdown.Item onClick={handleLogout} className="nav-link btn btn-warning" >
-                                        Sign Out
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            }
-                            {/* <Link to="/sign-in" className="nav-link btn btn-warning">Sign In</Link>
+                  <NavDropdown.Item onClick={handleLogout} className='nav-link btn btn-warning'>
+                    Sign Out
+                  </NavDropdown.Item>
+                </NavDropdown>}
+              {/* <Link to="/sign-in" className="nav-link btn btn-warning">Sign In</Link>
                         <Link to="/sign-up" className="nav-link btn btn-warning">Sign Up</Link> */}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
-export default Navigation;
+export default Navigation
