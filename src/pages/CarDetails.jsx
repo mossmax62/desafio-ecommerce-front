35c6ee70a994d96d8d9427c0151ef0bee99ec751
Diego Hernández <dieherram@gmail.com>
2024-08-29 20:47:55 -0400

    modified CarDetails.jsx, change text button

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -10,7 +10,7 @@ const CarDetails = () => {
   return (
     <div className='container d-flex vh-100 justify-content-center flex-column'>
       <ProductDetails />
-      <button onClick={() => addToCart(carSelected)} className='btn ms-auto rounded-pill' style={{ width: '15%' }}>Comprar</button>
+      <button onClick={() => addToCart(carSelected)} className='btn ms-auto btn btn-danger' style={{ width: '15%' }}>Add to Cart</button>
     </div>
 
   )
