b684d3044f3d2af1118b7b79fe300729c6a45240
Maximiliano Mussuto <mossmax62@gmail.com>
2024-09-10 20:43:52 -0300

    debug

--- src/components/products/AddProduct.jsx
+++ src/components/products/AddProduct.jsx
@@ -25,10 +25,10 @@ const AddProduct = () => {
     if (!img) newErrors.img = 'Image URL is required'
     if (!categoria) newErrors.categoria = 'Categoria is required'
     console.log('Justo antes de validacio de add product')
-    if (Object.keys(newErrors).length > 0) {
+    /* if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors)
       return
-    }
+    } */
     console.log('Justo antes de add product')
     addProduct({
       modelo,
