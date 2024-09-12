595f9ae9c0a340b9f747a109aa2cd3bba76c6265
Diego Hernández <dieherram@gmail.com>
2024-09-11 21:05:06 -0300

    rename property names

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -10,16 +10,16 @@ const ProductDetails = () => {
         <div className='card mb-3' style={{ width: '100%' }}>
           <div className='row g-0'>
             <div className='col-md-4 d-flex p-2'>
-              <img src={carSelected && carSelected.image} className='img-fluid rounded-start rounded' alt='...' />
+              <img src={carSelected && carSelected.img} className='img-fluid rounded-start rounded' alt='...' />
             </div>
             <div className='col-md-8'>
               <div className='card-body d-flex flex-column justify-content-between h-100 px-5'>
                 <div>
-                  <h1 className='card-title text-center m-0'>{carSelected && carSelected.model}</h1>
-                  <h2 className='card-title text-center m-0'>{carSelected && carSelected.make}</h2>
+                  <h1 className='card-title text-center m-0'>{carSelected && carSelected.marca}</h1>
+                  <h2 className='card-title text-center m-0'>{carSelected && carSelected.modelo}</h2>
                 </div>
-                <p className='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className='card-text text-end'>$ {carSelected && carSelected.horsepower}</p>
+                <p className='card-text'>{carSelected && carSelected.descripcion}</p>
+                <p className='card-text text-end'>$ {carSelected && carSelected.precio}</p>
               </div>
             </div>
           </div>
